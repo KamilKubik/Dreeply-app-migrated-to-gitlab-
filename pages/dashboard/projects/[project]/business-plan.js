43b1b660275be9ac552c880b805b6d13498662f0
KamilKubik <robekub15@wp.pl>
2021-04-30 14:27:18 +0200

    Delete some files due to request bytes limitation

--- pages/dashboard/projects/[project]/business-plan.js
+++
@@ -1,9 +0,0 @@
-import { Container } from 'postcss';
-import BusinessPlanContainer from '../../../../components/dashboard/startup/project/business-plan/Container';
-import BusinessPlanApp from '../../../../components/dashboard/startup/project/business-app/Page';
-
-const BusinessPlanPage = () => {
-  return <BusinessPlanApp />
-};
-
-export default BusinessPlanPage;
