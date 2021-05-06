b5f3f06ed0d3f21b765c9b87aa9697ccfb8ca283
KamilKubik <robekub15@wp.pl>
2021-05-06 23:13:23 +0200

    Fix import mismatch

--- pages/dashboard/projects/[project]/business-plan/index.js
+++ pages/dashboard/projects/[project]/business-plan/index.js
@@ -1,7 +1,7 @@
 import { Container } from 'postcss';
 import BusinessPlanContainer from '../../../../../components/dashboard/startup/project/business-plan/Container';
 import BusinessPlanApp from '../../../../../components/dashboard/startup/project/business-app/Page';
-import BusinessPlanPage from '../../../../../components/dashboard/startup/project/business-app/business-plan/BusinessPlanPage';
+// import BusinessPlanPage from '../../../../../components/dashboard/startup/project/business-app/business-plan/BusinessPlanPage';
 
 const BusinessPlan = () => {
   return <BusinessPlanApp />;
